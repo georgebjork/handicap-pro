eec9f4c80b1d2ed6aa4dc5c31ea990ead840b4de
George Bjork <georgebjork@outlook.com>
2021-09-22 22:53:27 -0700

    added max scores

--- src/Components/ScoreList.js
+++ src/Components/ScoreList.js
@@ -92,12 +92,99 @@ function ScoreList() {
             rating: 75.6,
             slope: 143
         },
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course1", 
+            score: 80, 
+            diff: 5,
+            rating: 75.6,
+            slope: 143
+        },
+
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course2", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
+
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course2", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
+
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course2", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
+
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course2", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
+
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course2", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
+
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course2", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
+
+        {
+            date: "1/1/2020", 
+            course: "Druids Glen Golf Course2", 
+            score: 81, 
+            diff: 7, 
+            rating: 75.6,
+            slope: 143
+        },
     
     ];
     // 
     const scoresList = scores.map(score => <Score score={score}/> )
     
-    if(scoresList.length > 10){
+    if(scoresList.length > 20){
         for(let i = scoresList.length; i > 10; i--){
             scoresList.pop();
         }
