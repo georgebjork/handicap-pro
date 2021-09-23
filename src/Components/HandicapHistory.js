9cd10c9af0b69d509a2337eb38f8d2cafc03e55b
George Bjork <georgebjork@outlook.com>
2021-09-22 22:52:54 -0700

    will display handicap history

---
+++ src/Components/HandicapHistory.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Container } from 'react-bootstrap'
+
+function HandicapHistory() {
+    return (
+        <div>
+            <Container id="friendsContainer" className="bg-secondary mt-4">
+                <h3 id="friendsText" class="display-6 p-1 text-white" align="center"> Handicap History </h3>
+            </Container>
+
+        </div>
+    )
+}
+
+export default HandicapHistory
